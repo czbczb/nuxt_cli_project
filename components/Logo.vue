9ce74a8dc98ed860e59328a3c24604168fabc3df
cuizb <czb@sinnet.com.cn>
2018-12-29 11:41:19 +0800

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="logoWrapper">
+    <div class="logo">我的第一个nuxt项目</div>
+  </div>
+</template>
+
+<style lang="less">
+.logoWrapper {
+  .logo {
+    text-align: center;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        hello world
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
